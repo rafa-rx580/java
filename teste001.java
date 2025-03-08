45e42cdaa472878cd3ba104e84dc57f72b36f31a
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-03-08 17:49:58 -0300

    first contact

---
+++ teste001.java
@@ -0,0 +1,22 @@
+package java;
+
+import java.util.ArrayList;
+
+public class teste001 {
+    byte by = 100;
+    short sh = 10000;
+    int in = 1000000000;
+    long lo = 1000000000000L;
+    float fl = 1.150f;
+    double d = 1.150000000;
+    String st = "Billionaire";
+    char ch = 'R';
+
+    ArrayList<String> nomes = new ArrayList<>();
+    //Construtor da classe
+    {
+        nomes.add("Rafael");
+        nomes.add("Felipe");
+        nomes.add("Clara");
+    }
+}
