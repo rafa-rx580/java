04c767bc8c1a59d5b926a99db5853015c2e8133e
rafa-rx580 <rafaelgomespereira62@gmail.com>
2025-11-25 21:55:44 -0300

    android-studio

---
+++ cursoemvideo/aulas/aula05/AppSwing/src/Screen.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Screen {
+    public Screen(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        jFrame.setSize(800, 500);
+        jFrame.setTitle("Aula 05");
+    }
+}
