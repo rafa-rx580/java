79efbba34f0c20e36c8750850f715b2cf984a46d
rafa-rx580 <rafaelgomespereira62@gmail.com>
2025-11-26 10:54:18 -0300

    Aula09 Comentário

--- devdojo/OlaDevDojo/src/academy/devdojo/maratona/introducao/OlaDevDojo.java
+++ devdojo/OlaDevDojo/src/academy/devdojo/maratona/introducao/OlaDevDojo.java
@@ -1,6 +1,16 @@
 package academy.devdojo.maratona.introducao;
 
 public class OlaDevDojo {
+    //Comentário de uma linha.
+    /*
+    Comentário de múltiplas linhas
+    */
+
+    /**
+     * Comentário usando JavaDoc
+     * É possível usar <strong>html</strong> dentro do daqui. Use CTRL ALT Q para formatar
+     */
+
     static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
