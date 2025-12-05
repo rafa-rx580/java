d889ff7bc84dde2d288a843aed0134295c0ba8f3
rafa-rx580 <rafaelgomespereira62@gmail.com>
2025-12-04 21:17:20 -0300

    Arrays multidimensionais primeira parte

---
+++ devdojo/OlaDevDojo/src/academy/devdojo/maratona/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 Months
+        // 31, 28, 31, 30, Days
+        int[][] dias = new int [2][2];
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+        }
+
+    }
+}
