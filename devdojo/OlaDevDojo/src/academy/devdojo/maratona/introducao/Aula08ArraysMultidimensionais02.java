d9d0c2a89c72288d9f36fc47813a162bd4889196
rafa-rx580 <rafaelgomespereira62@gmail.com>
2025-12-08 10:56:44 -0300

    Arrays multidimensionais parte 3

---
+++ devdojo/OlaDevDojo/src/academy/devdojo/maratona/introducao/Aula08ArraysMultidimensionais02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula08ArraysMultidimensionais02 {
+    static void main() {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[]{1, 2, 3};
+        arrayInt[2] = new int[6];
+
+        for (int[] arrayBase: arrayInt){
+            System.out.println("\n------------");
+            for (int num: arrayBase){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
